965559057ce820be6df08d21a6df220987a9f821
Ebisu02 <71516051+Ebisu02@users.noreply.github.com>
2021-03-17 13:53:58 +0600

    Update lab_7.cpp

--- lab_7.cpp
+++ lab_7.cpp
@@ -12,11 +12,29 @@ void BubbleSort_inv(int arr[], int size) {
 }
 
 
-
+int binarySearch(int arr[], int size, int key, int &c) { 
+	int left = -1;                 // left, right — левая и правая границы
+	int right = size;
+	int mid = 0;
+	while (left < right - 1) {   
+		++c;
+		mid = (left + right) / 2;        // mid — середина области поиска
+		if (arr[mid] > key) {
+			left = mid;
+		}
+		else {
+			right = mid;
+		}
+	}
+	if (arr[right] == key) 
+		return right;
+	else
+		return -1;
+}
 
 
 int pereborSearch(int arr[], int size, int key_what_we_find, int &c) {
-	c = 0; 
+	c = 0;
 	for (int i = 0; i < size; i++) {
 		++c;
 		if (arr[i] == key_what_we_find) {
@@ -51,7 +69,7 @@ void lab_7_1() {
 
 	// Perebor Search for N = 100
 	cout << "\n\nPerebor search:\n";
-	int c = 0; 
+	int c = 0;
 	if (pereborSearch(arr, size, key, c) != -1) {
 		cout << pereborSearch(arr, size, key, c) << " - index in what we have this key = " << key << "\n";
 		cout << "Compares = " << c << "\n";
@@ -108,7 +126,32 @@ void lab_7_1() {
 	return;
 }
 
+void lab_7_2() {
+	int** arr; 
+	const int size = 10;
+	arr = new int*[size];
+	int arr_size[size];
+	for (int i = 0; i < size; ++i) {
+		arr_size[i] = rand() % 10 + 1;
+		arr[i] = new int[arr_size[i]];
+	}
+	cout << "\nOur array:\n";
+	for (int i = 0; i < size; ++i) {
+		arr[i][0] = arr_size[i];
+		cout << arr[i][0] << " ";
+		for (int j = 1; j < arr_size[i]; ++j) {
+			arr[i][j] = rand() % 10;
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	return;
+}
+
 void lab_7() {
+	cout << "\nEx.1\n\n";
 	lab_7_1();
+	cout << "\nEx.2\n\n";
+	lab_7_2();
 	return;
 }
