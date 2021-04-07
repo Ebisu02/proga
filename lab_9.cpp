5167ed18073d6626234eeee7ca66f43b4939e9c4
Ebisu02 <71516051+Ebisu02@users.noreply.github.com>
2021-04-07 15:13:59 +0700

    Update lab_9.cpp

--- lab_9.cpp
+++ lab_9.cpp
@@ -5,9 +5,12 @@ const int size_of_grades = 4;
 typedef struct infoAboutStudent {
 	string surname;
 	int grade[size_of_grades];
+	struct infoAboutStudent *next;
 }list;
 
-void index_sort(list arr[], int size, int indexArr[]) {
+template <typename T>
+
+void index_sort(T arr[], int size, int indexArr[]) {
 	for (int i = 0; i < size - 1; i++) {
 		for (int j = 0; j < size - i - 1; j++) {
 			if (arr[indexArr[j]].surname[0] > arr[indexArr[j + 1]].surname[0]) {
@@ -19,17 +22,29 @@ void index_sort(list arr[], int size, int indexArr[]) {
 }
 
 void lab_9_1() {
-	string str = { "ABC" };
-	cout << str[0] << str[1] << str[2];
+	const int size_for_examples = 4;
+	string examples_Surname[size_for_examples] = { { "Pertsev" }, { "Ivanov" }, { "Petrov" }, { "Fedorov" } };
+	int examples_Grades[size_for_examples] = { { 2 }, { 3 }, { 4 }, { 5 } };
+	list List;
+	srand(time(nullptr));
+	const int size = 5;
+	int link;
+	while (int i = 0 < size) {
+		for (int j = 0; j < size_of_grades; ++j) {
+			List.grade[j] = examples_Grades[rand() % size_for_examples];
+		}
+		List.surname = examples_Surname[rand() % size_for_examples];
+
+	}
 	return;
-} 
+}
 
 void lab_9_2() {
-	
-	return; 
+
+	return;
 }
 
 void lab_9() {
 	lab_9_1();
-	return; 
+	return;
 }
